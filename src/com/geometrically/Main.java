899eb1f56d703f9acfb5ceb010ef2add088168a8
Geometrically <jaiagr123@gmail.com>
2017-12-20 22:48:57 -0700

    Efficiency Improvements

--- src/com/geometrically/Main.java
+++ src/com/geometrically/Main.java
@@ -23,61 +23,48 @@ SOFTWARE.
 package com.geometrically;
 
 import javax.xml.bind.DatatypeConverter;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static String enteredHash;
+    private static final String dir = System.getProperty("user.dir");
 
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-        final String dir = System.getProperty("user.dir");
-        //Scanner for encrypted password
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Hash(SHA-256): ");
-        enteredHash =scanner.nextLine();
-        //File Reading
-        FileReader fr = new FileReader(dir + "\\src\\dictionary.txt");
-        BufferedReader in = new BufferedReader(fr);
-        String str;
-        int encryptIndex = 0;
-
-        List<String> list = new ArrayList<String>();
-        while((str = in.readLine()) != null){
-            //add strings
-            list.add(str);
-        }
-        String[] stringArr = list.toArray(new String[0]);
-        System.out.println( "Things read:" + Arrays.toString(stringArr));
-
-        //Encryption
-        List<String> encryptList = new ArrayList<String>();
-        for(String s : stringArr){
-            //encrypt into encrypted arrays
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(s.getBytes(StandardCharsets.UTF_8));
-
-            encryptList.add(DatatypeConverter.printHexBinary(hash));
-
-        }
-        String[] encryptArr = encryptList.toArray(new String[0]);
-
-        System.out.println("Encrypted Strings:" + Arrays.toString(encryptArr));
-        //Add Correspondence to array values
-        for(int i=0;i<encryptArr.length;i++){
-            if(encryptArr[i].equals(enteredHash)){
-                encryptIndex = i;
-                break;
+        String enteredHash =scanner.nextLine();
+        long startTime = System.currentTimeMillis();
+        boolean found = false;
+        String pathName = dir + "\\" + args[0];
+        try(BufferedReader in = new BufferedReader(new FileReader(pathName))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                try {
+                    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                    byte[] hash1 = digest.digest(line.getBytes(StandardCharsets.UTF_8));
+                    String encrypted = DatatypeConverter.printHexBinary(hash1);
+                    String encrypted1 = null;
+                    if(enteredHash.equals(encrypted)){
+                        long endTime = System.currentTimeMillis();
+                        System.out.println("Password:\"" + line + "\" ---> Program took " +(endTime - startTime) + " ms");
+                        found = true;
+                    }
+                } catch (NoSuchAlgorithmException e){
+                    e.printStackTrace();
+                }
             }
+        } catch(IOException e){
+            e.printStackTrace();
         }
-        System.out.println("Deencrypted Hash: " + stringArr[encryptIndex]);
+        if(!found){
+            long endTime = System.currentTimeMillis();
+            System.out.println("The password could not be found ---> Program took " +(endTime - startTime) + " ms");
+        }
     }
 }
 
